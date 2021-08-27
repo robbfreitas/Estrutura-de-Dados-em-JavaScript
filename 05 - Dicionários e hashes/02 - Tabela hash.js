2864b9450a3f3c50384cc5d0f4d68ad36fe59c4e
RobbFreitas <robertocompromissos@gmail.com>
2021-08-27 11:20:46 -0300

    HashTable

---
+++ 05 - Dicionários e hashes/02 - Tabela hash.js
@@ -0,0 +1,89 @@
+// Classe HashTable, também conhecida como HashMap.
+// hashing consiste em encontrar um valor em uma ED
+// o mais rápido possível.
+// A função de Hash que usaremos erá a mais comum, a
+// chamada função de hash lose-lose, onde simplesmente
+// os valores ASCII de cada caractere da chave são somados
+
+// Criando uma classe HashTable
+
+function defaultToString(item) {
+    if (item === null) {
+        return 'NULL';
+    } else if (item === undefined) {
+        return 'UNDEFINED';
+    } else if (typeof item === 'string' || item instanceof String) {
+        return `${item}`;
+    }
+    return item.toString();
+}
+class ValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+    toString() {
+        return `[#${this.key}: ${this.value}]`;
+    }
+}
+
+class HashTable {
+    constructor(toStrFn = defaultToString) {
+        this.toStrFn = toStrFn;
+        this.table = {};
+    }
+    // Criando uma função de hash
+    loseloseHashCode(key) {
+        if (typeof key === 'number') {
+            return key;
+        }
+        const tableKey = this.toStrFn(key);
+        let hash = 0;
+        for (let i = 0; i < tableKey.length; i++) {
+            hash += tableKey.charCodeAt(i);
+        }
+        return hash % 37;
+    }
+    hashCode(key) {
+        return this.loseloseHashCode(key);
+    }
+    // Inserindo uma chave e um valor na tabela hash
+    put(key, value) {
+        if (key != null && value != null) {
+            const position = this.hashCode(key);
+            this.table[position] = new ValuePair(key, value);
+            return true;
+        }
+        return false;
+    }
+    // Obtendo um valor da tabela hash
+    get(key) {
+        const valuePair = this.table[this.hashCode(key)];
+        return valuePair = null ? undefined : valuePair.value;
+    }
+    // Removendo um valor da tabela hash
+    remove(key) {
+        const hash = this.hashCode(key);
+        const valuePair = this.table[hash];
+        if (valuePair != null) {
+            delete this.table[hash];
+            return true;
+        }
+        return false;
+    }
+}
+
+// Usando a classe HashTable
+
+const hash = new HashTable();
+hash.put('Gandalf', 'gandalf@email.com');
+hash.put('John', 'johnsnow@email.com');
+hash.put('Tyrion', 'tyrion@email.com');
+console.log(hash.hashCode('Gandalf') + ' - Gandalf')
+console.log(hash.hashCode('John') + ' - John')
+console.log(hash.hashCode('Tyrion') + ' - Tyrion')
+// 19 - Gandalf
+// 29 - John
+// 16 - Tyrion
+
+// Tratando colisões nas tabelas hash
