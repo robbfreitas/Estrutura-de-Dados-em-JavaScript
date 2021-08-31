3892100b96eae6b81aca3cf3155d8ef452ebff56
RobbFreitas <robertocompromissos@gmail.com>
2021-08-31 20:03:49 -0300

    fatorial iterativo

---
+++ 06 - Recursão/02 - Algoritmos recursivos famosos.js
@@ -0,0 +1,11 @@
+// Fatorial iterativo
+
+function factorialIterative(number) {
+    if (number < 0) return undefined;
+    let total = 1;
+    for (let n = number; n > 1; n--) {
+        total = total * n;
+    }
+    return total;
+}
+console.log(factorialIterative(5)); // 120
