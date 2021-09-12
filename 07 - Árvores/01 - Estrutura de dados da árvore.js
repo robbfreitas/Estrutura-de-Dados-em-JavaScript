9dffc79d992942b79dc76ca7d5d2aee374f3a512
RobbFreitas <robertocompromissos@gmail.com>
2021-09-12 09:42:12 -0300

    Estrutura de dados da árvore

---
+++ 07 - Árvores/01 - Estrutura de dados da árvore.js
@@ -0,0 +1,7 @@
+// Uma árvore  é um modelo abstrato de uma estrutura hierárquica.
+// o exemplo mais comum de uma árvore na vida real seria o de uma
+// árvore genealógica ou o programa de uma empresa.
+
+// Uma árvore é constituída de nós com um relacionamento pai-filho.
+// Todo nó tem um pai (exceto o primeiro nó no topo) e zero ou mais
+// filhos.
